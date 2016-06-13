93e295c4bd6e9700ab9d3dcb348d8b8cd7e29bc3
sdossettswift <sdossettswift@gmail.com>
2016-06-13 04:38:51 -0500

    give department raise

--- department.rb
+++ department.rb
@@ -1,23 +1,43 @@
 # require "./employee"
 
 class Department
-    attr_accessor :dept, :employees_in_dept, :new_hire
+    attr_accessor :department_name, :employees_in_dept, :raise_eligible, :raise_ineligible
 
-    @employees_in_dept = Array.new
+    def initialize
+        @employees_in_dept = Array.new
+        @department_name = "Department Name"
+        @raise_eligible = Array.new
+        @raise_ineligible = Array.new
+    end
 
-
-    def dept
-        @dept
-
+    def department_name
+        @department_name
     end
 
     def add_employee(new_hire)
-        @employees_in_dept =+(new_hire)
-
+        @employees_in_dept << new_hire
+        @raise_eligible << new_hire
     end
 
     def list_employees
-        puts @employee_list.inspect
+        puts @employees_in_dept.inspect
     end
 
+    def who_is_raise_eligible?
+        @employees_in_dept.each do |employee|
+            if employee.rating == "unsatisfactory"
+                @raise_ineligible << employee
+            else
+                @raise_ineligible << employee
+            end
+            # puts @raise_eligible.inspect
+        end
+
+        def give_department_raise(percentage)
+            @raise_eligible.each do |employee|
+                percentage = 1.0 + (percentage.to_i/100.0)
+                employee.salary = (employee.salary * percentage).to_i
+                            end
+        end
+    end
 end
--- test_employee_reviews.rb
+++ test_employee_reviews.rb
@@ -45,19 +45,52 @@ class EmployeeReviewTest < MiniTest::Test
     end
 
     def test_add_department
-        @department.dept = "Development"
-        assert_equal "Development", @department.dept
+        @department.department_name = "Development"
+        assert_equal "Development", @department.department_name
     end
 
     def test_department_has_people
         @department.add_employee("Sarah")
-        assert_includes ["Sarah"], @department.employees_in_dept
+        assert_equal ["Sarah"], @department.employees_in_dept
 
     end
 
     def test_give_employee_raise
         @employee.give_employee_raise(50)
         assert_equal 75_000, @employee.salary
+    end
+    def test_give_department_raise
+
+        annie = Employee.new
+        annie.name = "Annie"
+        annie.email = "little_orphan@annie.com"
+        annie.phone = "713.555.0000"
+        annie.salary= 50_000
+        annie.rating = "satisfactory"
+
+        brutus = Employee.new
+        brutus.name = "Brutus"
+        brutus.email = "backstabber@romantimes.com"
+        brutus.phone = "713.555.0001"
+        brutus.salary= 50_000
+        brutus.rating = "unsatisfactory"
+
+        cynthia = Employee.new
+        cynthia.name = "Cynthia"
+        cynthia.email = "best_grandmother@swift.com"
+        cynthia.phone = "713.555.0000"
+        cynthia.salary= 50_000
+        cynthia.rating = "satisfactory"
+
+        @department.add_employee(annie)
+        @department.add_employee(brutus)
+        @department.add_employee(cynthia)
+
+        @department.who_is_raise_eligible?
+
+        @department.give_department_raise(10)
+
+        assert_equal 55_000, annie.salary
     end
 
 end
